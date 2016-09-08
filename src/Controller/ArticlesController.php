6cbc9c627bd60254a0ca93e2dc5c4f19d315c2fb
Himgiri <himgiri.aggarwal94@gmail.com>
2016-09-08 15:19:17 +0530

    first commit

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,61 @@
+<?php
+
+// src/Controller/ArticlesController.php
+
+namespace App\Controller;
+
+class ArticlesController extends AppController
+{
+	// public function index()
+    // {
+        // $articles = $this->Articles->find('all');
+        // $this->set(compact('articles'));
+    // }
+	public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent('Flash'); // Include the FlashComponent
+    }
+
+    public function index()
+    {
+        $this->set('articles', $this->Articles->find('all'));
+    }
+
+    public function view($id)
+    {
+        $article = $this->Articles->get($id);
+        $this->set(compact('article'));
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->data);
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        $this->set('article', $article);
+    }
+	
+	public function edit($id = null)
+	{
+		$article = $this->Articles->get($id);
+		if ($this->request->is(['post', 'put'])) {
+			$this->Articles->patchEntity($article, $this->request->data);
+			if ($this->Articles->save($article)) {
+				$this->Flash->success(__('Your article has been updated.'));
+				return $this->redirect(['action' => 'index']);
+			}
+			$this->Flash->error(__('Unable to update your article.'));
+		}
+		$this->set('article', $article);
+	}
+}
+
+?>
